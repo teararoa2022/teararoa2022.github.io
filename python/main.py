7aa1fc1505963165caa6ee0d9e4252e2a67dbfdb
Stefano Savare <stefano.savare@gmail.com>
2022-10-14 11:16:02 +0200

    Added TMB recap
    Signed-off-by: Stefano Savare <stefano.savare@gmail.com>

--- python/main.py
+++ python/main.py
@@ -1,6 +1,9 @@
 import datetime
 import sys
 
+import geojson
+from geojson import MultiLineString
+
 sys.path.append(".")
 
 
@@ -10,7 +13,7 @@ from typing import List, Set
 
 from stravalib import Client
 
-from python.config import ALREADY_PROCESSED_ACTIVITIES
+from python.config import ALREADY_PROCESSED_ACTIVITIES, ASSETS_DIR, REGEX_TO_TAGS
 from python.strava import StravaActivity, StravaActivityFilter, StravaInterface
 from python.strava_post import StravaPost
 
@@ -26,6 +29,52 @@ def export_refresh_token_to_github_env_variables(client: Client):
     env_file = os.getenv("GITHUB_ENV")
     with open(env_file, "a") as f:
         f.write(f"STRAVA_REFRESH_TOKEN={client.refresh_token}")
+
+
+def generate_posts(strava_interface: StravaInterface) -> List[StravaPost]:
+    activity_filter = StravaActivityFilter(
+        remove_already_processed_activities=True,
+        only_selected_activity_types=True,
+        only_taggable_activities=True,
+    )
+    activities = strava_interface.get_activities(
+        after=datetime.datetime.today() - datetime.timedelta(days=60),
+        activity_filter=activity_filter,
+    )
+    strava_posts = [StravaPost(activity) for activity in activities]
+    for post in strava_posts:
+        post.generate()
+    update_already_processed_activities(activity_filter.already_processed_activities, activities)
+    return strava_posts
+
+
+def _compute_data_for_tag(tag_dir):
+    data = []
+    for folder in sorted(tag_dir.glob("*")):
+        if folder.is_dir():
+            geojson_file = folder / "gps_data.geojson"
+            with open(geojson_file, "r") as f:
+                data.append(geojson.load(f)["coordinates"][::10])
+    return data
+
+
+def _export_data_to_geojson(data, tag_dir, tag):
+    mls = MultiLineString(data)
+    with open(tag_dir / f"{tag}.geojson", "w") as f:
+        geojson.dump(mls, f)
+
+
+def _add_tag_recap_geojson(tag):
+    tag_dir = ASSETS_DIR / tag
+    if tag_dir.exists() and tag_dir.is_dir():
+        data = _compute_data_for_tag(tag_dir)
+        _export_data_to_geojson(data, tag_dir, tag)
+
+
+def add_tags_recap_geojson():
+    tags = set.union(*(set(tags) for tags in REGEX_TO_TAGS.values()))
+    for tag in tags:
+        _add_tag_recap_geojson(tag)
 
 
 if __name__ == "__main__":
@@ -45,19 +94,8 @@ if __name__ == "__main__":
     )
     strava_interface.refresh_client_access_token(STRAVA_REFRESH_TOKEN, TOKEN_EXPIRES_AT)
 
-    activity_filter = StravaActivityFilter(
-        remove_already_processed_activities=True,
-        only_selected_activity_types=True,
-        only_taggable_activities=True,
-    )
-    activities = strava_interface.get_activities(
-        after=datetime.datetime.today() - datetime.timedelta(days=60),
-        activity_filter=activity_filter,
-    )
-    for activity in activities:
-        strava_post = StravaPost(activity)
-        strava_post.generate()
-    update_already_processed_activities(activity_filter.already_processed_activities, activities)
+    strava_posts = generate_posts(strava_interface)
+    add_tags_recap_geojson()
 
     if not LOCAL:
         export_refresh_token_to_github_env_variables(client)
--- tmb.md
+++ tmb.md
@@ -3,3 +3,12 @@ layout: tmb
 title: TMB 2022
 permalink: /tmb/
 ---
+
+
+{% leaflet_map {"zoom" : 10,
+                "center": [45.8414835, 6.878885000000001],
+                "divId" : "map_size" } %}
+    {% leaflet_geojson "/assets/tmb/tmb.geojson" %}
+{% endleaflet_map %}
+
+<br />
